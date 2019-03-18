b78cc0ee5680313f98b2ab399a55da92d346500d
Ronaldo Rigoni <rrigoni@gmail.com>
2019-03-18 12:25:38 -0300

    Adjusted imports

--- src/main/java/com/sendwyre/invoice/invoice/model/service/implementation/BitcoinWalletService.java
+++ src/main/java/com/sendwyre/invoice/invoice/model/service/implementation/BitcoinWalletService.java
@@ -17,7 +17,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
